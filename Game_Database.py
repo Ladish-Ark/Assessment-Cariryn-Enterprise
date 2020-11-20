db65e600fb435170699827504c457f61a7dd57db
snazzy <pectoralmodz@gmail.com>
2020-11-20 16:11:59 +0000

    View Character Page Complete

--- Game_Database.py
+++ Game_Database.py
@@ -3,17 +3,16 @@ import mysql.connector
 
 app = Flask(__name__, static_url_path='/static')
 
-#connecting to local database. This is obvs terrible security. ( discussed as part of lead in to U7)
-#vetdb = mysql.connector.connect(user='Vets', password='Vets123456',
-#                              host='127.0.0.1', database='vets',
-#                              auth_plugin='mysql_native_password')
+gamedb = mysql.connector.connect(user='callum', password='GT67phn@',
+                              host='192.168.0.34', database='game',
+                              auth_plugin='mysql_native_password')
 
 
 #Code runs if index.html called (root)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if "viewcharacter" in request.form:
-        return render_template('/view_char.html')
+        return render_template('/view_char.html', data=viewchar())
     elif "viewitems" in request.form:
         return render_template('/view_item.html')
     elif "viewencounters" in request.form:
@@ -30,9 +29,8 @@ def index():
 ########################################################################################################################################################################################################
 
 #Code runs if adding a new owner (submit pressed on owner.html page)
-@app.route('/owner', methods=['GET', 'POST'])
-def owner():
-  #Takes details from form and crafts a sql statement  
+@app.route('/character_edit', methods=['GET', 'POST'])
+def character():  
   if "submit" in request.form:
           details = request.form
           Surname = details['Surname']
@@ -40,9 +38,10 @@ def owner():
           Address = details['Address']
           Address2 = details['Address2']
           Phone = details['Phone']
-          cur = vetdb.cursor()
-          cur.execute("INSERT INTO owner(Surname, FirstName, Address, Address2, Phone) VALUES (%s, %s,%s, %s,%s)", (Surname, FirstName, Address, Address2,Phone))
-          vetdb.commit()
+          cur = gamedb.cursor()
+          # Need to finish line 
+          #cur.execute("INSERT INTO player_characters(first_name, last_name, raceID_FK1, classID_FK1, alignmentID_FK1, level, strength, brawn, agility, mettle, craft, insight, wits, resolve, life, armourID_FK1, protectionID_FK1) VALUES (%s, %s, %s, %s, %s %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,)", (Surname, FirstName, Address, Address2,Phone))
+          gamedb.commit()
           cur.close()
           
   elif "cancel" in request.form:
@@ -69,6 +68,12 @@ def petadd():
   elif "cancel" in request.form:
         pass
   return render_template('/index.html')
+
+def viewchar():
+    cur = gamedb.cursor()
+    cur.execute("SELECT first_name, last_name, race.name, classes.classes, alignment.type, level, strength, brawn, agility, mettle, craft, insight, wits, resolve, life, armour.name, protection.name FROM player_characters, race, classes, alignment, armour, protection where player_characters.raceID_FK1=race.raceID and player_characters.classID_FK1=classes.classID and player_characters.alignmentID_FK1=alignment.alignmentID and player_characters.armourID_FK1=armour.armourID and player_characters.protectionID_FK1=protection.protectionID;")
+    data = cur.fetchall()
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
